00290e431e890c335b27f00e4a34e496c47b313f
sajib689 <jibonraja11@gmail.com>
2024-01-31 19:34:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var person = {};
+person.name = "John";
+person.age = 30;
+console.log(person)
+let str = "Mississippi";
+console.log(str.indexOf("i", 3));
+const myObject = {
+    a: 1,
+    b: 2,
+    c: 3
+  };
+  
+  for (let prop in myObject) {
+    console.log(myObject[prop]);
+  }
+  const x = ""
+if(!x.length){
+console.log("Hey")
+}else{
+console.log("Hi")
+}
